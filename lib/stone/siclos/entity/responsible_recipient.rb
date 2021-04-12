cb83994f5f2b46617dc1ab087c43727bc5174c05
Marcos Junior <marcos.junior@rhsoftware.com.br>
2021-04-12 15:18:50 -0300

    Fix repeated attribute in responsible recipient

--- lib/stone/siclos/entity/responsible_recipient.rb
+++ lib/stone/siclos/entity/responsible_recipient.rb
@@ -4,8 +4,8 @@ module Stone
   module Siclos
     module Entity
       class ResponsibleRecipient < Base
-        attribute :name, Types::Coercible::String
-        attribute :cell_phone, Types::OnlyNumbersString.optional.default(nil)
+        attribute :email, Types::Coercible::String
+        attribute :cell_phone, Types::OnlyNumbersString
         attribute :document_number, Types::Coercible::String.optional.default(nil)
         attribute :name, Types::Coercible::String.optional.default(nil)
       end
